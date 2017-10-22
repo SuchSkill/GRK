bd9026330009ed2ea0a40aaa1b2a9201d3e0dd70
Yevhen Shymko <imjust93@gmail.com>
2017-10-22 18:22:35 +0200

    Histogramm

--- lab2/task5_Histogram/script.js
+++ lab2/task5_Histogram/script.js
@@ -9,11 +9,15 @@ function setup() {
     createCanvas(256,256);
     img.resize(256,256);
     img.loadPixels();
-    stroke(0);
+    background(0)
+    stroke(255);
     var table = new Array(256);
     table.fill(0);
 
     img.filter('gray');
+    // img.updatePixels();
+    img.loadPixels();
+
     for(x=0;x<img.width;x++)
         for(y=0;y<img.height;y++) {
             pos=4*(y*img.width+x);
@@ -21,19 +25,21 @@ function setup() {
             // console.log(m);
             table[m]= table[m]+1;
         }
-
+    for(i =0; i<256; i++){
+        table[i] = table[i]/5;
+    }
+    // line(0,256,0,60)
     for(i =0; i<256; i++) {
         if(table[i] > 256){
-            line(i, 256, i, 0);
+            // line(i, 256, i, 0);
+            console.log(i);
 
         }else {
-            line(i, table[i], i, 0);
+            line(i, 0, i, 256-table[i]);
+            console.log(table[i]);
         }
-        console.log(table[i]);
     }
-    for(i = 0; i<256;i++)
-    console.log(i + " " +table[i]);
-    // console.log(sum);
-    // img.updatePixels();
+    // for(i = 0; i<256;i++)
+    // console.log(i + " " +table[i]);
     // image(img, 0, 0);
 }
