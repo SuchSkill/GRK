b738b874892396f12fe6edfad8297965a34fd9da
Yevhen Shymko <imjust93@gmail.com>
2017-11-16 12:05:06 +0100

    lab3/task3

--- lab3/task3/script.js
+++ lab3/task3/script.js
@@ -1,3 +1,5 @@
+console.log(12);
+
 function setup() {
     createCanvas(512, 512);
     // pixelDensity(1)
@@ -7,7 +9,6 @@ function setup() {
     //     set_pixel(i,i,0)
     // }
 }
-
 var x0 = -1;
 var y0 = -1;
 var x1 = -1;
@@ -41,35 +42,28 @@ function mouseReleased() {
 
 function set_pixel(x, y, c) {
     idx = (y * 512 + x) * 4;
-    pixels[idx] = c;
+    pixels[idx] = -c;
     pixels[idx + 1] = c;
-    pixels[idx + 2] = c;
+    pixels[idx + 2] = 0;
     pixels[idx + 3] = 255;
 }
 
 function draw_line() {
     dx = x1 - x0;
     dy = y1 - y0;
-    Dp = (2*dy)-dx;
-    Deq  = 2*dy;
-    Dinc = (2*dy-(2*dx));
     a = dy / dx;
     b = y0 - (a * x0);
-
-    Dhat = (2*dy)-dy;
-    for (x = 0; x < 512; x++) {
-        for (y = 0; y < 512; y++) {
-            midpoint = (dy/dx) - (1/2);
-
-            set_pixel(x, y, D);
+    Dp = (2 * dy) - dx;
+    Deq = 2*dy;
+    Dinc = (2*dy) - dx;
+    D = Dp;
+    for (x = x0,y = y0; x <= x1; x++) {
+            set_pixel(x, y, 0);
             if(D<0){
                 D+=Deq;
             }else{
                 D+=Dinc;
-                y+=1;
+                y++;
             }
-        }
     }
-
-
 }
